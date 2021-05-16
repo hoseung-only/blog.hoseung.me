87c220761c433273efbcf5952ef5eeefec2406cd
HoseungJang <hsjang.dev@gmail.com>
2021-05-16 17:15:49 +0900

    add colors

---
+++ src/constants/color.ts
@@ -0,0 +1,18 @@
+export enum Color {
+  Blue300 = "#58ACFA",
+  Blue200 = "#81BEF7",
+  Blue100 = "#A9D0F5",
+  Green300 = "#81F79F",
+  Green200 = "#A9F5BC",
+  Green100 = "#CEF6D8",
+  Black100 = "#1C1C1C",
+  Black100Transparency50 = "rgba(28, 28, 28, 0.5)",
+  Black50 = "#585858",
+  White = "#FFFFFF",
+  Grey300 = "#BDBDBD",
+  Grey200 = "#D8D8D8",
+  Grey100 = "#E6E6E6",
+  Grey50 = "#F2F2F2",
+  Grey10 = "#FAFAFA",
+  Red = "#FF0000",
+}
