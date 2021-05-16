e708d6221a9ac217b55defdacfc6341133cbaed9
HoseungJang <hsjang.dev@gmail.com>
2021-05-16 17:08:20 +0900

    config react-router

---
+++ src/routes/RouteSwitch.tsx
@@ -0,0 +1,30 @@
+import { Suspense } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { routes, RouteSpec, Paths } from "./routeSpec";
+
+export function RouteSwitch() {
+  return (
+    <Switch>
+      {routes.map((routeSpec) => (
+        <ResponsiveRoute key={routeSpec.name} {...routeSpec} />
+      ))}
+    </Switch>
+  );
+}
+
+function ResponsiveRoute({ path, exact, component: Component, placeholder: Placeholder }: RouteSpec) {
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={(match) => {
+        return (
+          <Suspense fallback={Placeholder ? <Placeholder /> : null}>
+            <Component {...match} />
+          </Suspense>
+        );
+      }}
+    />
+  );
+}
---
+++ src/routes/routeSpec.tsx
@@ -0,0 +1,20 @@
+import _ from "lodash";
+import { ComponentType } from "react";
+import { RouteComponentProps } from "react-router";
+
+type PageName = "";
+
+export interface RouteSpec {
+  name: PageName;
+  path: string;
+  exact?: boolean;
+  component: ComponentType<any> | ComponentType<RouteComponentProps<any, any, any>>;
+  placeholder?: ComponentType<any>;
+}
+
+export const routes: RouteSpec[] = [];
+
+export const Paths = _.chain(routes)
+  .map(({ name, path }) => [name, path] as const)
+  .fromPairs()
+  .value() as { [key in typeof routes[number]["name"]]: string };
