03c6429b2126939068c4ca447985fc3d25626770
HoseungJang <hsjang.dev@gmail.com>
2021-05-16 17:18:37 +0900

    add Label

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Label = {
+  R12: styled.div`
+    font-size: 12px;
+    font-weight: 300;
+  `,
+  M12: styled.div`
+    font-size: 12px;
+    font-weight: 500;
+  `,
+  B12: styled.div`
+    font-size: 12px;
+    font-weight: 700;
+  `,
+  R16: styled.div`
+    font-size: 16px;
+    font-weight: 300;
+  `,
+  M16: styled.div`
+    font-size: 16px;
+    font-weight: 500;
+  `,
+  B16: styled.div`
+    font-size: 16px;
+    font-weight: 700;
+  `,
+  R20: styled.div`
+    font-size: 20px;
+    font-weight: 300;
+  `,
+  M20: styled.div`
+    font-size: 20px;
+    font-weight: 500;
+  `,
+  B20: styled.div`
+    font-size: 20px;
+    font-weight: 700;
+  `,
+  R24: styled.div`
+    font-size: 24px;
+    font-weight: 300;
+  `,
+  M24: styled.div`
+    font-size: 24px;
+    font-weight: 500;
+  `,
+  B24: styled.div`
+    font-size: 24px;
+    font-weight: 700;
+  `,
+};
