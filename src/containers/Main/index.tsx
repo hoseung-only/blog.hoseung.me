bdd1075b1a241d12002ce306867df827775653be
HoseungJang <hsjang.dev@gmail.com>
2021-05-19 23:56:59 +0900

    add Main

---
+++ src/containers/Main/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export function Main() {
+  return <S.Container></S.Container>;
+}
+
+export function MainPlaceholder() {
+  return <S.Container></S.Container>;
+}
+
+const S = {
+  Container: styled.div``,
+};
