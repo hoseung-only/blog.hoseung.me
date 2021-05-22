2ce28c2cf39f5c5adb835a6d78e089c270a31c7f
HoseungJang <hsjang.dev@gmail.com>
2021-05-22 22:42:11 +0900

    add PlatformSwitch

---
+++ src/components/PlatformSwitch/index.tsx
@@ -0,0 +1,14 @@
+import { usePlatform, Platform } from "../../hooks/usePlatform";
+
+export function PlatformSwitch({ desktop, mobile }: { desktop(): JSX.Element; mobile(): JSX.Element }) {
+  const platform = usePlatform();
+
+  switch (platform) {
+    case Platform.Desktop: {
+      return desktop();
+    }
+    case Platform.Mobile: {
+      return mobile();
+    }
+  }
+}
