713ee0f046605a505e3ad6f84d99aa311e106c89
HoseungJang <hsjang.dev@gmail.com>
2021-05-23 21:11:06 +0900

    add return values, use tsx instead of ts

---
+++ src/containers/Main/hooks/usePosts.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+import { usePaginatedAPIQuery } from "../../../hooks/useAPIQuery";
+
+export function usePosts() {
+  const { data, loadMore, isLoading, canLoadMore } = usePaginatedAPIQuery("getPostsByCursor", {});
+
+  const { ref, inView } = useInView({ initialInView: false });
+
+  useEffect(() => {
+    if (inView) {
+      loadMore?.();
+    }
+  }, [loadMore, inView]);
+
+  return {
+    posts: data,
+    loadMore,
+    isLoading,
+    canLoadMore,
+    loadingTriggerElement: loadMore !== null ? <div ref={ref} /> : null,
+  };
+}
