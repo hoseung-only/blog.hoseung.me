630222e46eabbc148c95791e3b01304c5ecebbf3
HoseungJang <hsjang.dev@gmail.com>
2021-06-02 23:26:54 +0900

    use loadingTriggerRef

--- src/containers/Main/Desktop/index.tsx
+++ src/containers/Main/Desktop/index.tsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 import { usePosts } from "../hooks/usePosts";
 
 export function Main() {
-  const { posts, loadingTriggerElement } = usePosts();
+  const { posts, loadingTriggerRef } = usePosts();
 
   return (
     <S.Container>
       {posts.map((post) => (
         <div>{post.title}</div>
       ))}
-      {loadingTriggerElement}
+      <div ref={loadingTriggerRef} />
     </S.Container>
   );
 }
