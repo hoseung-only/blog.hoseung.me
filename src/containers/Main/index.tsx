cadf2edcf0d3d3a4f0029bfb43d91567119ae60f
HoseungJang <hsjang.dev@gmail.com>
2021-06-04 01:06:32 +0900

    apply OG

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,9 +1,15 @@
-import { PlatformSwitch } from "../../components/Shared/PlatformSwitch";
+import { OG } from "../../components/Shared/OG";
 import { Main as Desktop, MainPlaceholder as DesktopPlaceholder } from "./Desktop";
 import { Main as Mobile, MainPlaceholder as MobilePlaceholder } from "./Mobile";
+import { PlatformSwitch } from "../../components/Shared/PlatformSwitch";
 
 export function Main() {
-  return <PlatformSwitch desktop={() => <Desktop />} mobile={() => <Mobile />} />;
+  return (
+    <>
+      <OG />
+      <PlatformSwitch desktop={() => <Desktop />} mobile={() => <Mobile />} />
+    </>
+  );
 }
 
 export function MainPlaceholder() {
