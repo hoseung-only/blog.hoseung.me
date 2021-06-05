2b6ea6ce35ecbcc8019841d2923647327ee26619
HoseungJang <hsjang.dev@gmail.com>
2021-06-05 22:54:04 +0900

    remove unused colors

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -2,11 +2,7 @@ export enum Color {
   Blue300 = "#58ACFA",
   Blue200 = "#81BEF7",
   Blue100 = "#A9D0F5",
-  Green300 = "#81F79F",
-  Green200 = "#A9F5BC",
-  Green100 = "#CEF6D8",
   Black100 = "#1C1C1C",
-  Black100Transparency50 = "rgba(28, 28, 28, 0.5)",
   Black50 = "#585858",
   White = "#FFFFFF",
   Grey300 = "#BDBDBD",
@@ -14,5 +10,4 @@ export enum Color {
   Grey100 = "#E6E6E6",
   Grey50 = "#F2F2F2",
   Grey10 = "#FAFAFA",
-  Red = "#FF0000",
 }
