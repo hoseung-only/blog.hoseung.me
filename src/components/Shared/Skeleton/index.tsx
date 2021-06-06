666b2120ff3f719865468dfd2c4fd088b6973532
HoseungJang <hsjang.dev@gmail.com>
2021-06-06 23:54:35 +0900

    fix animation

--- src/components/Shared/Skeleton/index.tsx
+++ src/components/Shared/Skeleton/index.tsx
@@ -5,13 +5,13 @@ import { Color } from "../../../constants/color";
 const animation = css`
   @keyframes loading {
     0% {
-      background-color: ${Color.Grey50};
-    }
-    50% {
       background-color: ${Color.Grey10};
     }
-    100% {
+    50% {
       background-color: ${Color.Grey50};
+    }
+    100% {
+      background-color: ${Color.Grey10};
     }
   }
 
