33445fcaab63fafce4848239c414e64214517a3b
HoseungJang <hsjang.dev@gmail.com>
2021-08-03 23:56:25 +0900

    add grid

---
+++ src/components/Shared/Grid/index.tsx
@@ -0,0 +1,55 @@
+import { PropsWithChildren, createContext, useContext } from "react";
+import styled, { css } from "styled-components";
+
+interface GridProps {
+  columnSize: number;
+  columnMargin: number;
+  rowMargin: number;
+}
+
+const GridContext = createContext<GridProps | null>(null);
+
+export function Grid({ children, ...value }: PropsWithChildren<GridProps>) {
+  return (
+    <GridContext.Provider value={value}>
+      <S.Grid>{children}</S.Grid>
+    </GridContext.Provider>
+  );
+}
+
+export function GridItem({ children }: PropsWithChildren<{}>) {
+  const context = useContext(GridContext);
+  if (!context) {
+    throw new Error("GridItem must be used within Grid");
+  }
+  return <S.GridItem {...context}>{children}</S.GridItem>;
+}
+
+const S = {
+  Grid: styled.ul`
+    width: 100%;
+
+    display: flex;
+    flex-flow: row wrap;
+
+    margin: 0;
+    padding: 0;
+
+    list-style: none;
+  `,
+  GridItem: styled.li<GridProps>`
+    ${({ columnSize, columnMargin, rowMargin }) => css`
+      width: calc((100% - ${columnMargin * (columnSize - 1)}px) / ${columnSize});
+
+      margin-right: ${columnMargin}px;
+
+      &:nth-child(${columnSize}n) {
+        margin-right: 0;
+      }
+
+      &:nth-child(-n + ${columnSize}) {
+        margin-bottom: ${rowMargin}px;
+      }
+    `}
+  `,
+};
