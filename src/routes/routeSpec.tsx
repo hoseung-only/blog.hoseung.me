fcd15f6443228bd19438f6a9f3a1e582951f0323
HoseungJang <hsjang.dev@gmail.com>
2021-08-05 00:55:40 +0900

    allow to pass path as array

--- src/routes/routeSpec.tsx
+++ src/routes/routeSpec.tsx
@@ -1,14 +1,14 @@
-import _ from "lodash";
 import { ComponentType } from "react";
 import { RouteComponentProps } from "react-router";
 
 import { Main, MainPlaceholder } from "../containers/Main";
+import { PostShow, PostShowPlaceholder } from "../containers/PostShow";
 
 type PageName = "Main" | "PostShow";
 
 export interface RouteSpec {
   name: PageName;
-  path: string;
+  path: string | string[];
   exact?: boolean;
   component: ComponentType<any> | ComponentType<RouteComponentProps<any, any, any>>;
   placeholder?: ComponentType<any>;
@@ -17,14 +17,16 @@ export interface RouteSpec {
 export const routes: RouteSpec[] = [
   {
     name: "Main",
-    path: "/",
+    path: ["/", "/categories"],
     exact: true,
     component: Main,
     placeholder: MainPlaceholder,
   },
+  {
+    name: "PostShow",
+    path: "/posts/:postId",
+    exact: true,
+    component: PostShow,
+    placeholder: PostShowPlaceholder,
+  },
 ];
-
-export const Paths = _.chain(routes)
-  .map(({ name, path }) => [name, path] as const)
-  .fromPairs()
-  .value() as { [key in typeof routes[number]["name"]]: string };
