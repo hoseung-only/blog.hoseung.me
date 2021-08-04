ff474d983715252ed1b5b94992c33f2916c49c14
HoseungJang <hsjang.dev@gmail.com>
2021-08-05 01:40:17 +0900

    fix header

--- src/components/Desktop/Header/index.tsx
+++ src/components/Desktop/Header/index.tsx
@@ -1,22 +1,11 @@
 import styled from "styled-components";
 
-import { Color } from "../../../constants/color";
-import { externalLinks } from "../../../constants/externalLinks";
-
 import { Label } from "../../Shared/Label";
 
 export function Header() {
   return (
     <S.Container>
-      <Label.M24 className="title">장호승 개발 블로그</Label.M24>
-      <div className="link-list">
-        {externalLinks.map((link) => (
-          <a key={link.name} className="link" href={link.to}>
-            <link.icon className="icon" />
-            <Label.B20>{link.name}</Label.B20>
-          </a>
-        ))}
-      </div>
+      <Label.M24>장호승 개발 블로그</Label.M24>
     </S.Container>
   );
 }
@@ -27,45 +16,6 @@ const S = {
     height: 100px;
 
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    border-bottom: 1px solid ${Color.Grey100};
-
-    > .title {
-      margin-bottom: 12px;
-    }
-
-    > .link-list {
-      height: 25px;
-
-      display: flex;
-      align-items: center;
-
-      > .link {
-        height: 100%;
-
-        display: flex;
-        align-items: center;
-
-        color: ${Color.Blue300};
-        text-decoration: none;
-
-        &:hover {
-          color: ${Color.Blue100};
-        }
-
-        &:not(:last-child) {
-          margin-right: 12px;
-        }
-
-        > .icon {
-          width: 22px;
-          height: 22px;
-
-          margin-right: 2px;
-        }
-      }
-    }
+    align-items: center;
   `,
 };
--- src/components/Mobile/Header/index.tsx
+++ src/components/Mobile/Header/index.tsx
@@ -1,21 +1,11 @@
 import styled from "styled-components";
 
-import { Color } from "../../../constants/color";
-import { externalLinks } from "../../../constants/externalLinks";
-
 import { Label } from "../../Shared/Label";
 
 export function Header() {
   return (
     <S.Container>
-      <Label.M20 className="title">장호승 개발 블로그</Label.M20>
-      <div className="link-list">
-        {externalLinks.map((link) => (
-          <a key={link.name} className="link" href={link.to}>
-            <link.icon className="icon" />
-          </a>
-        ))}
-      </div>
+      <Label.M20>장호승 개발 블로그</Label.M20>
     </S.Container>
   );
 }
@@ -26,35 +16,6 @@ const S = {
     height: 60px;
 
     display: flex;
-    justify-content: space-between;
     align-items: center;
-
-    border-bottom: 1px solid ${Color.Grey100};
-
-    > .link-list {
-      height: 25px;
-
-      display: flex;
-      align-items: center;
-
-      > .link {
-        height: 100%;
-
-        display: flex;
-        align-items: center;
-
-        color: ${Color.Blue300};
-        text-decoration: none;
-
-        &:not(:last-child) {
-          margin-right: 10px;
-        }
-
-        > .icon {
-          width: 25px;
-          height: 25px;
-        }
-      }
-    }
   `,
 };
