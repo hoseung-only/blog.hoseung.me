30ebe21543cddb609664e4bc9468686d681bf75f
HoseungJang <hsjang.dev@gmail.com>
2021-08-05 11:50:38 +0900

    fix tablet media query

--- src/constants/media.ts
+++ src/constants/media.ts
@@ -1,5 +1,5 @@
 export enum Media {
   Desktop = "@media screen and (min-width: 1024px)",
-  Tablet = "@media screen and (max-width: 1023px)",
+  Tablet = "@media screen and (min-width: 769px) and (max-width: 1023px)",
   Mobile = "@media screen and (max-width: 768px)",
 }
