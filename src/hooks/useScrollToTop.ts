2144c28d48d1a3a0cf2249e8e0cd0911e060fcda
HoseungJang <hsjang.dev@gmail.com>
2021-08-06 03:05:07 +0900

    add disallowedPaths

--- src/hooks/useScrollToTop.ts
+++ src/hooks/useScrollToTop.ts
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import { useHistory } from "react-router";
 
+const disallowedPaths = new Set(["/", "/categories"]);
+
 export function useScrollToTop() {
   const history = useHistory();
 
   useEffect(() => {
     history.listen(() => {
-      if (history.action !== "POP") {
+      if (history.action !== "POP" && !disallowedPaths.has(window.location.pathname)) {
         window.scrollTo({ top: 0 });
       }
     });
