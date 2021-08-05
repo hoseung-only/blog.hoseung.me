cabb4d552ff477813f2a530376811713a9fd45e1
HoseungJang <hsjang.dev@gmail.com>
2021-08-05 11:38:31 +0900

    add font

---
+++ src/components/Font/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Font = {
+  Regular: styled.div`
+    font-weight: 300;
+  `,
+  Medium: styled.div`
+    font-weight: 500;
+  `,
+  Bold: styled.div`
+    font-weight: 700;
+  `,
+};
