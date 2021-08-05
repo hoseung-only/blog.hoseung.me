14e34769b91265b72cc65483a01422e693405876
HoseungJang <hsjang.dev@gmail.com>
2021-08-05 18:34:02 +0900

    add useCategoryPosts

---
+++ src/containers/Category/hooks/useCategoryPosts.ts
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useInView } from "react-intersection-observer";
+
+import { usePaginatedAPIQuery } from "../../../hooks/useAPIQuery";
+
+export function useCategoryPosts() {
+  const { categoryId } = useParams<{ categoryId: string }>();
+  const { data, loadMore, isLoading, canLoadMore } = usePaginatedAPIQuery(
+    "getCategoryPostsByCursor",
+    { id: Number(categoryId) },
+    { suspense: true }
+  );
+
+  const { ref, inView } = useInView({ initialInView: false });
+
+  useEffect(() => {
+    if (inView) {
+      loadMore?.();
+    }
+  }, [loadMore, inView]);
+
+  return {
+    posts: data,
+    loadMore,
+    isLoading,
+    canLoadMore,
+    loadingTriggerRef: loadMore !== null ? ref : null,
+  };
+}
