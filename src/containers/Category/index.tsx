34543b03c8b96c3dc118f9e56ee8416a3b932026
HoseungJang <hsjang.dev@gmail.com>
2021-08-05 18:33:07 +0900

    add Category

---
+++ src/containers/Category/index.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { Font } from "../../components/Font";
+
+import { useCategoryPosts } from "./hooks/useCategoryPosts";
+
+import { PostList, PostListPlaceholder } from "../../components/PostList";
+
+import { Color } from "../../constants/color";
+
+export function Category() {
+  const { posts, loadingTriggerRef } = useCategoryPosts();
+  return (
+    <S.Container>
+      <Font.Regular className="title">카테고리 포스트</Font.Regular>
+      <PostList posts={posts} />
+      <div ref={loadingTriggerRef} />
+    </S.Container>
+  );
+}
+
+export function CategoryPlaceholder() {
+  return (
+    <S.Container>
+      <Font.Regular className="title">카테고리 포스트</Font.Regular>
+      <PostListPlaceholder itemCount={12} />
+    </S.Container>
+  );
+}
+
+const S = {
+  Container: styled.div`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 40px 0;
+
+    box-sizing: border-box;
+
+    > .title {
+      padding: 16px;
+      margin-bottom: 20px;
+
+      box-sizing: border-box;
+
+      box-shadow: inset 0 -2px 0 0 ${Color.Blue300};
+
+      font-size: 2.4rem;
+      color: ${Color.Blue300};
+    }
+  `,
+};
