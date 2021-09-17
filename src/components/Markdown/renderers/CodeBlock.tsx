138cd4503a3ac5c791733ff2a0a2c4b323d8b143
HoseungJang <hsjang.dev@gmail.com>
2021-09-18 02:12:41 +0900

    use cjs

--- src/components/Markdown/renderers/CodeBlock.tsx
+++ src/components/Markdown/renderers/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export function CodeBlock({ className, children }: { className?: string; children: ReactNode }) {
   return (
