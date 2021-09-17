db6c5ff01a002ce628bed5c3dd6da30ca097accc
HoseungJang <hsjang.dev@gmail.com>
2021-09-18 02:12:57 +0900

    apply next link

--- src/components/Markdown/renderers/AnchorBlock.tsx
+++ src/components/Markdown/renderers/AnchorBlock.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import Link from "next/link";
 import styled from "styled-components";
 
 import { Color } from "../../../constants/color";
@@ -7,9 +7,9 @@ export function AnchorBlock(props: any) {
   return props.href.startsWith("#") || props.href.startsWith("http") ? (
     <S.Container href={props.href}>{props.children}</S.Container>
   ) : (
-    <S.Container as={Link} to={props.href}>
-      {props.children}
-    </S.Container>
+    <Link href={props.href}>
+      <S.Container>{props.children}</S.Container>
+    </Link>
   );
 }
 
