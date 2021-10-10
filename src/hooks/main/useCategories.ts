3f880bbb224e2b7fe032c639d47e07d2572fdad3
HoseungJang <hsjang.dev@gmail.com>
2021-10-11 01:51:32 +0900

    sort categories alphabetically

--- src/hooks/main/useCategories.ts
+++ src/hooks/main/useCategories.ts
@@ -1,5 +1,8 @@
+import { useMemo } from "react";
+
 import { useAPIQuery } from "../common/useAPIQuery";
 
 export function useCategories() {
-  return useAPIQuery("getAllCategories", { count: 12 }, { suspense: true }).data.data;
+  const categories = useAPIQuery("getAllCategories", { count: 12 }, { suspense: true }).data.data;
+  return useMemo(() => categories.sort((a, b) => (a.name > b.name ? 1 : -1)), [categories]);
 }
