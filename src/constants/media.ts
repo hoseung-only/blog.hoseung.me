09444ab184e36cde6b79739557b83eda8565b5e3
HoseungJang <hsjang.dev@gmail.com>
2021-12-08 03:25:09 +0900

    fix media

--- src/constants/media.ts
+++ src/constants/media.ts
@@ -1,5 +1,5 @@
 export enum Media {
   Desktop = "@media screen and (min-width: 1024px)",
-  Tablet = "@media screen and (min-width: 769px) and (max-width: 1023px)",
-  Mobile = "@media screen and (max-width: 768px)",
+  Tablet = "@media screen and (min-width: 750px) and (max-width: 1023px)",
+  Mobile = "@media screen and (max-width: 749px)",
 }
